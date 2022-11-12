14b9319b6bdef2d815270513d3fb0df39a5d14ab
Manomk <113567762+Manomk@users.noreply.github.com>
2022-11-12 18:10:24 +0530

    Update Weather.py

--- Project Development Phase/Sprint 1/Weather.py
+++ Project Development Phase/Sprint 1/Weather.py
@@ -1,35 +1,15 @@
 # Python code
 
-# IMPORT SECTION STARTS
+import requests as reqs
 
-import weather
-from datetime import datetime as dt
-
-# IMPORT SECTION ENDS
-# -----------------------------------------------
-# UTILITY LOGIC SECTION STARTS
-def processConditions(myLocation,APIKEY,localityInfo):
-    weatherData = weather.get(myLocation,APIKEY)
-
-    finalSpeed = localityInfo["usualSpeedLimit"] if "rain" not in weatherData else localityInfo["usualSpeedLimit"]/2
-    finalSpeed = finalSpeed if weatherData["visibility"]>35 else finalSpeed/2
-
-    if(localityInfo["hospitalsNearby"]):
-        # hospital zone
-        doNotHonk = True
-    else:
-        if(localityInfo["schools"]["schoolZone"]==False):
-            # neither school nor hospital zone
-            doNotHonk = False
-        else:
-            # school zone
-            now = [dt.now().hour,dt.now().minute]
-            activeTime = [list(map(int,_.split(":"))) for _ in localityInfo["schools"]["activeTime"]]
-            doNotHonk = activeTime[0][0]<=now[0]<=activeTime[1][0] and activeTime[0][1]<=now[1]<=activeTime[1][1]
-
-    return({
-        "speed" : finalSpeed,
-        "doNotHonk" : doNotHonk
-    })
-
-# UTILITY LOGIC SECTION ENDS
+def get(myLocation,APIKEY):
+    apiURL = "https://api.openweathermap.org/data/2.5/weather?q={mylocation}&appid={APIkey}"
+    responseJSON = (reqs.get(apiURL)).json()
+    returnObject = {
+         "temperature" : responseJSON['main']['temp'] - 273.15,
+         "weather" : [responseJSON['weather'][_]['main'].lower() for _ in range(len(responseJSON['weather']))],
+         "visibility" : responseJSON['visibility']/100, #visibility in percentage where 10km is 100% and 0km is 0%
+    }
+    if("rain" in responseJSON):
+        returnObject["rain"] = [responseJSON["rain"][key] for key in responseJSON["rain"]]
+    return(returnObject)
